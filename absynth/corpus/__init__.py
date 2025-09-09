5feee08e5ceeb490fb4a9ceb99990b71ce183098
Nura J <nura.aljaafari@postgrad.manchester.ac.uk>
2025-09-09 09:57:54 +0100

    test

--- absynth/corpus/__init__.py
+++ absynth/corpus/__init__.py
@@ -2,3 +2,4 @@ from .synthetic_corpus_generator import SyntheticCorpusGenerator, SynthCorpus
 from .corpus_evaluator import CorpusEvaluator
 
 __all__ = ['SynthCorpus', 'SyntheticCorpusGenerator', 'CorpusEvaluator']
+
